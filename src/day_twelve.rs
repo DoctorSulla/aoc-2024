ff1550ddb44aa7098061375ee377eefc7fb64de4
= <=>
2024-12-17 21:59:21 +0000

    D12 P2 complete - Ugliest solution ever

--- src/day_twelve.rs
+++ src/day_twelve.rs
@@ -1,7 +1,8 @@
 #[derive(Debug, PartialEq, Clone, Copy)]
-pub enum GardenStatus {
+pub enum GardenState {
     Unvisited(char),
     Visited(char),
+    Border,
 }
 
 #[derive(Debug, PartialEq)]
@@ -13,26 +14,27 @@ pub enum Direction {
     None,
 }
 
-pub fn get_farm(file: &str) -> Vec<Vec<GardenStatus>> {
-    let mut map: Vec<Vec<GardenStatus>> = vec![];
+pub fn get_farm(file: &str) -> Vec<Vec<GardenState>> {
+    let mut map: Vec<Vec<GardenState>> = vec![];
     for line in file.lines() {
-        map.push(line.chars().map(|x| GardenStatus::Unvisited(x)).collect());
+        map.push(line.chars().map(|x| GardenState::Unvisited(x)).collect());
     }
     map
 }
 
-pub fn walk_farm(map: &mut Vec<Vec<GardenStatus>>) {
+pub fn walk_farm(map: &mut Vec<Vec<GardenState>>) {
     let mut le_total = 0;
-    let mut current_char: GardenStatus;
+    let mut budget_total = 0;
+    let mut current_char: GardenState;
     for row_index in 0..map.len() {
         for col_index in 0..map[row_index].len() {
             match map[row_index][col_index] {
-                GardenStatus::Unvisited(v) => {
+                GardenState::Unvisited(v) => {
                     let mut area: usize = 1;
                     let mut perimeter: usize = 0;
-                    let mut sides: usize = 0;
+                    let mut corners: usize = 0;
                     current_char = map[row_index][col_index];
-                    map[row_index][col_index] = GardenStatus::Visited(v);
+                    map[row_index][col_index] = GardenState::Visited(v);
                     get_contiguous_region(
                         current_char,
                         map,
@@ -40,61 +42,164 @@ pub fn walk_farm(map: &mut Vec<Vec<GardenStatus>>) {
                         col_index,
                         &mut area,
                         &mut perimeter,
-                        &mut sides,
-                        Direction::None,
+                        &mut corners,
                     );
-                    println!(
-                        "The area for the contiguous region {:?} is {:?},the perimeter is {}, and the number of sides is {}",
-                        current_char, area, perimeter, sides
-                    );
+                    // println!(
+                    //     "The area for the contiguous region {:?} is {:?},the perimeter is {}, and the number of sides is {}",
+                    //     current_char, area, perimeter, corners
+                    // );
                     le_total += area * perimeter;
+                    budget_total += area * corners;
                 }
-                GardenStatus::Visited(_) => {}
+                _ => {}
             }
         }
     }
     println!("The total is {}", le_total);
+    println!("The budget total is {}", budget_total);
 }
 
 fn get_contiguous_region(
-    current_char: GardenStatus,
-    map: &mut Vec<Vec<GardenStatus>>,
+    current_char: GardenState,
+    map: &mut Vec<Vec<GardenState>>,
     row_index: usize,
     col_index: usize,
     area: &mut usize,
     perimeter: &mut usize,
-    sides: &mut usize,
-    current_direction: Direction,
+    corners: &mut usize,
 ) {
     match current_char {
-        GardenStatus::Unvisited(v) => {
+        GardenState::Unvisited(v) => {
+            // Inside corner if two adjacent sides are the same and diagonal is not
+            // Outside corner if two adjacent sides are not the same
+            let mut north = GardenState::Border;
+            let mut south = GardenState::Border;
+            let mut east = GardenState::Border;
+            let mut west = GardenState::Border;
+            let mut northwest = GardenState::Border;
+            let mut southwest = GardenState::Border;
+            let mut northeast = GardenState::Border;
+            let mut southeast = GardenState::Border;
+
+            // Bound checks
+            if row_index != 0 {
+                north = map[row_index - 1][col_index];
+            }
+            if row_index != 0 && map[row_index].get(col_index + 1).is_some() {
+                northeast = map[row_index - 1][col_index + 1];
+            }
+            if row_index != 0 && col_index != 0 {
+                northwest = map[row_index - 1][col_index - 1];
+            }
+            if map.get(row_index + 1).is_some() {
+                south = map[row_index + 1][col_index];
+            }
+            if map.get(row_index + 1).is_some()
+                && map.get(row_index + 1).unwrap().get(col_index + 1).is_some()
+            {
+                southeast = map[row_index + 1][col_index + 1];
+            }
+            if col_index != 0 && map.get(row_index + 1).is_some() {
+                southwest = map[row_index + 1][col_index - 1];
+            }
+            if col_index != 0 {
+                west = map[row_index][col_index - 1];
+            }
+            if map[row_index].get(col_index + 1).is_some() {
+                east = map[row_index][col_index + 1];
+            }
+
+            if (north == GardenState::Unvisited(v) || north == GardenState::Visited(v))
+                && (west == GardenState::Unvisited(v) || west == GardenState::Visited(v))
+                && northwest != GardenState::Unvisited(v)
+                && northwest != GardenState::Visited(v)
+            {
+                *corners += 1;
+            }
+
+            if (north == GardenState::Unvisited(v) || north == GardenState::Visited(v))
+                && (east == GardenState::Unvisited(v) || east == GardenState::Visited(v))
+                && northeast != GardenState::Unvisited(v)
+                && northeast != GardenState::Visited(v)
+            {
+                *corners += 1;
+            }
+
+            if (south == GardenState::Unvisited(v) || south == GardenState::Visited(v))
+                && (west == GardenState::Unvisited(v) || west == GardenState::Visited(v))
+                && southwest != GardenState::Unvisited(v)
+                && southwest != GardenState::Visited(v)
+            {
+                *corners += 1;
+            }
+
+            if (south == GardenState::Unvisited(v) || south == GardenState::Visited(v))
+                && (east == GardenState::Unvisited(v) || east == GardenState::Visited(v))
+                && southeast != GardenState::Unvisited(v)
+                && southeast != GardenState::Visited(v)
+            {
+                *corners += 1;
+            }
+
+            if south != GardenState::Visited(v)
+                && south != GardenState::Unvisited(v)
+                && east != GardenState::Visited(v)
+                && east != GardenState::Unvisited(v)
+            {
+                *corners += 1;
+            }
+
+            if south != GardenState::Visited(v)
+                && south != GardenState::Unvisited(v)
+                && west != GardenState::Visited(v)
+                && west != GardenState::Unvisited(v)
+            {
+                *corners += 1;
+            }
+
+            if north != GardenState::Visited(v)
+                && north != GardenState::Unvisited(v)
+                && east != GardenState::Visited(v)
+                && east != GardenState::Unvisited(v)
+            {
+                *corners += 1;
+            }
+
+            if north != GardenState::Visited(v)
+                && north != GardenState::Unvisited(v)
+                && west != GardenState::Visited(v)
+                && west != GardenState::Unvisited(v)
+            {
+                *corners += 1;
+            }
+
             // Perimiter - Start with 4 and remove 1 for each adjacent plot of the same type
             let mut perimeter_addition = 4;
 
             if row_index != 0
                 && (map[row_index - 1][col_index] == current_char
-                    || map[row_index - 1][col_index] == GardenStatus::Visited(v))
+                    || map[row_index - 1][col_index] == GardenState::Visited(v))
             {
                 perimeter_addition -= 1;
             }
             // Check down
             if row_index != map.len() - 1
                 && (map[row_index + 1][col_index] == current_char
-                    || map[row_index + 1][col_index] == GardenStatus::Visited(v))
+                    || map[row_index + 1][col_index] == GardenState::Visited(v))
             {
                 perimeter_addition -= 1;
             }
             // Check left
             if col_index != 0
                 && (map[row_index][col_index - 1] == current_char
-                    || map[row_index][col_index - 1] == GardenStatus::Visited(v))
+                    || map[row_index][col_index - 1] == GardenState::Visited(v))
             {
                 perimeter_addition -= 1;
             }
             // Check right
             if col_index != map[0].len() - 1
                 && (map[row_index][col_index + 1] == current_char
-                    || map[row_index][col_index + 1] == GardenStatus::Visited(v))
+                    || map[row_index][col_index + 1] == GardenState::Visited(v))
             {
                 perimeter_addition -= 1;
             }
@@ -104,11 +209,8 @@ fn get_contiguous_region(
 
             // Check up
             if row_index != 0 && map[row_index - 1][col_index] == current_char {
-                if current_direction != Direction::Up {
-                    *sides += 1;
-                }
                 *area += 1;
-                map[row_index - 1][col_index] = GardenStatus::Visited(v);
+                map[row_index - 1][col_index] = GardenState::Visited(v);
                 get_contiguous_region(
                     current_char,
                     map,
@@ -116,17 +218,13 @@ fn get_contiguous_region(
                     col_index,
                     area,
                     perimeter,
-                    sides,
-                    Direction::Up,
+                    corners,
                 );
             }
             // Check down
             if row_index != map.len() - 1 && map[row_index + 1][col_index] == current_char {
-                if current_direction != Direction::Down {
-                    *sides += 1;
-                }
                 *area += 1;
-                map[row_index + 1][col_index] = GardenStatus::Visited(v);
+                map[row_index + 1][col_index] = GardenState::Visited(v);
                 get_contiguous_region(
                     current_char,
                     map,
@@ -134,17 +232,13 @@ fn get_contiguous_region(
                     col_index,
                     area,
                     perimeter,
-                    sides,
-                    Direction::Down,
+                    corners,
                 );
             }
             // Check left
             if col_index != 0 && map[row_index][col_index - 1] == current_char {
-                if current_direction != Direction::Left {
-                    *sides += 1;
-                }
                 *area += 1;
-                map[row_index][col_index - 1] = GardenStatus::Visited(v);
+                map[row_index][col_index - 1] = GardenState::Visited(v);
                 get_contiguous_region(
                     current_char,
                     map,
@@ -152,17 +246,13 @@ fn get_contiguous_region(
                     col_index - 1,
                     area,
                     perimeter,
-                    sides,
-                    Direction::Left,
+                    corners,
                 );
             }
             // Check right
             if col_index != map[0].len() - 1 && map[row_index][col_index + 1] == current_char {
-                if current_direction != Direction::Right {
-                    *sides += 1;
-                }
                 *area += 1;
-                map[row_index][col_index + 1] = GardenStatus::Visited(v);
+                map[row_index][col_index + 1] = GardenState::Visited(v);
                 get_contiguous_region(
                     current_char,
                     map,
@@ -170,11 +260,10 @@ fn get_contiguous_region(
                     col_index + 1,
                     area,
                     perimeter,
-                    sides,
-                    Direction::Right,
+                    corners,
                 );
             }
         }
-        GardenStatus::Visited(_) => {}
+        _ => {}
     }
 }
